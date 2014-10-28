886a9306376e94f7737a1b7a75d47d8d86dae468
Philipp Kretzschmar <philipp.kretzschmar@gmail.com>
2014-10-28 15:50:52 +0100

    minor version

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,4 @@
 /*global module:false*/
-
-
 module.exports = function(grunt) {
   "use strict";
   grunt.initConfig({
@@ -10,7 +8,11 @@ module.exports = function(grunt) {
         updateConfigs: [],
         commit: true,
         commitMessage: 'Release v%VERSION%',
-        commitFiles: ['package.json', 'bower.json'],
+        commitFiles: [
+          'package.json',
+          'bower.json',
+          'dist/HnBrowserDetector.min.js'
+        ],
         createTag: true,
         tagName: 'v%VERSION%',
         tagMessage: 'Version %VERSION%',
